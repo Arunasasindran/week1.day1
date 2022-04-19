8f282351cf3cbc70a6d306b6ccb808d53cf52831
Arunasasindran <103830341+Arunasasindran@users.noreply.github.com>
2022-04-19 15:02:30 +0530

    Add files via upload

---
+++ CheckPositiveOrNegative.java
@@ -0,0 +1,21 @@
+package week1.day1.assignments;
+
+public class CheckPositiveOrNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = -35;
+
+		if (i < 0) {
+			System.out.println("The number "+i+" is Negative");
+		}
+		else if (i > 0) {
+			System.out.println("The number "+i+" is Positive");
+		} 
+		
+		else {
+			System.out.println("The number "+i+" is nor Negative or Positive");
+		}
+	}
+
+}
---
+++ NegativeToPositive.java
@@ -0,0 +1,17 @@
+package week1.day1.assignments;
+
+public class NegativeToPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1 = -40;
+
+		if (num1 < 0) {
+			num1 = 40;
+			System.out.println("The number -40 is converted to 40");
+		}
+
+	}
+
+}
